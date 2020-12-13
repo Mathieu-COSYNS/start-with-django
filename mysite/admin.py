be5a432db06feca0eccd0c7ad9623178630cdae0
Mathieu COSYNS <mathieucosyns@gmail.com>
2020-12-13 14:34:45 +0100

    add models

--- mysite/admin.py
+++ mysite/admin.py
@@ -1,4 +1,11 @@
 from django.contrib import admin
-from .models import User
+from .models import Comment, CommentLike, CommentReport, Confession, ConfessionLike, ConfessionReport, Hashtag, User
 
 admin.site.register(User)
+admin.site.register(Confession)
+admin.site.register(Comment)
+admin.site.register(Hashtag)
+admin.site.register(ConfessionLike)
+admin.site.register(CommentLike)
+admin.site.register(ConfessionReport)
+admin.site.register(CommentReport)
--- mysite/models.py
+++ mysite/models.py
@@ -1,5 +1,4 @@
 from django.db import models
-from django.db.models.fields import CharField
 
 
 class User(models.Model):
@@ -10,3 +9,66 @@ class User(models.Model):
 
     def __str__(self):
         return self.email
+
+
+class Confession(models.Model):
+    title = models.CharField(max_length=120)
+    content = models.TextField(max_length=1000)
+    hashtags = models.ManyToManyField("Hashtag", blank=True)
+
+    def __str__(self):
+        if len(self.title) > 40:
+            return self.title[:40] + "..."
+        return self.title
+
+
+class Comment(models.Model):
+    content = models.TextField(max_length=300)
+    confession = models.ForeignKey("Confession", on_delete=models.CASCADE)
+    author = models.ForeignKey("User", on_delete=models.CASCADE)
+
+    def __str__(self):
+        if len(self.content) > 40:
+            return self.content[:40] + "..."
+        return self.content
+
+
+class Hashtag(models.Model):
+    name = models.CharField(max_length=30)
+
+    def __str__(self):
+        return self.name
+
+
+class ConfessionLike(models.Model):
+    user = models.ForeignKey("User", on_delete=models.CASCADE)
+    confession = models.ForeignKey("Confession", on_delete=models.CASCADE)
+    positive = models.BooleanField()
+
+    def __str__(self):
+        return self.user + " " + self.confession + " " + self.positive
+
+
+class CommentLike(models.Model):
+    user = models.ForeignKey("User", on_delete=models.CASCADE)
+    comment = models.ForeignKey("Comment", on_delete=models.CASCADE)
+    positive = models.BooleanField()
+
+    def __str__(self):
+        return self.user + " " + self.comment + " " + self.positive
+
+
+class ConfessionReport(models.Model):
+    confession = models.ForeignKey("Confession", on_delete=models.CASCADE)
+    count = models.PositiveIntegerField()
+
+    def __str__(self):
+        return self.confession + " " + self.count
+
+
+class CommentReport(models.Model):
+    comment = models.ForeignKey("Comment", on_delete=models.CASCADE)
+    count = models.PositiveIntegerField()
+
+    def __str__(self):
+        return self.comment + " " + self.count
